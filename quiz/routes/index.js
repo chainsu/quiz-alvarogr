0ce93b2c829261d3b87112d3191a86c3af9be50a
Alvaro Gutierrez <chainsu@gmail.com>
2015-07-18 09:38:40 +0200

    Resubir archivos modificados

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -16,4 +16,8 @@ router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 module.exports = router;
