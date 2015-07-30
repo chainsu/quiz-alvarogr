0298c79d873f0d21ec2832e1bd868fcc06ca3524
Alvaro Gutierrez <chainsu@gmail.com>
2015-07-30 19:02:33 +0200

    Soporte HTTPS

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -31,7 +31,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
-router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 	                                    sessionController.loginRequired, commentController.publish);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 	                                    sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
   res.render('author', {errors: []});
